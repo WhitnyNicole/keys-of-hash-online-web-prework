a70c05cf99ca10e3300862a9726c12bcb4e8b14b
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 01:25:38 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    
+  def keys_of(*arguments)
+    keys_array = []
+    arguments.each do |arguments|
+      self.each do |animal_name, origin_country|
+        if origin_country == arguments
+          keys_array << animal_name  
+        end
+      end
+    end
+    keys_array
   end
 end
